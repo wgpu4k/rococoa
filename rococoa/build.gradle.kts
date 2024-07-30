549f89a10e3ca39f8a05cf0701fa660581d73de0
Alexandre Mommers <alexandre.mommers@gmail.com>
2024-07-30 19:25:14 +0200

    initial version

---
+++ rococoa/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	kotlin("jvm")
+	id("publish")
+}
+
+dependencies {
+	implementation(libs.jna)
+	implementation(libs.jnaPlatform)
+	implementation(libs.cglib)
+}
+
+kotlin {
+	compilerOptions {
+		allWarningsAsErrors = true
+	}
+}
+
+val buildNativeLibraryTask = tasks.register<Exec>("buildNativeLibrary") {
+	commandLine(
+		"xcodebuild",
+		"-project",
+		"rococoa.xcodeproj",
+		"-target",
+		"librococoa-test",
+		"-configuration",
+		"Release"
+	)
+}
+
+val copyNativeLibraryTask = tasks.register<Copy>("copyFile") {
+	dependsOn(buildNativeLibraryTask)
+	from("build/Release/librococoa.dylib")  // source file
+	into("src/main/resources/darwin")  // destination directory
+}
+
+tasks.named("processResources") {
+	if (Platform.os == Os.MacOs) dependsOn(copyNativeLibraryTask)
+}
+
+
